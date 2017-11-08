ae99b823c0956679ccfabcd17dc7b7fa0fa08a66
LkeMitchll <mail@luke.fail>
2017-11-08 09:03:45 +0000

    Add basic header and footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Header from './header.js'
 import Gallery from './gallery.js'
+import Footer from './footer.js'
+import styles from './app.css'
 
 class App extends React.Component {
   render() {
     return (
-      <Gallery />
+      <section className={styles.wrapper}>
+        <Header />
+        <Gallery />
+        <Footer />
+      </section>
     )
   }
 }
---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>An interroban.gg project</p>
+        <p>
+          Set in Tiempos Text & Courier Prime<br/>
+          Data from instagram<br/>
+          Source code hosted at GitHub
+        </p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
