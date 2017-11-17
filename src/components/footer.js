1a4c7739bdbd5e4c90901d503788e59648cf9848
LkeMitchll <mail@luke.fail>
2017-11-17 15:40:09 +0000

    Add footer links and styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,10 @@ class Footer extends React.Component {
       <footer>
         <p className={styles.text}>An interroban.gg project</p>
         <p className={styles.text}>
-          Set in Tiempos Text & Courier Prime<br/>
-          Data from instagram<br/>
-          Source code hosted at GitHub
+          Set in <a className={styles.link} href="https://klim.co.nz/retail-fonts/tiempos-text/">Tiempos Text</a> &amp; <a className={styles.link} href="https://quoteunquoteapps.com/courierprime/">Courier Prime</a>.
+          <br/>
+          Data from <a href="https://www.instagram.com/developer/">Instagram</a><br/>
+          Source code hosted at <a className={styles.link} href="https://github.com/LkeMitchll/aperture">GitHub</a>.
         </p>
       </footer>
     )
